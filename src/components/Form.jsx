10e98c23b0dfbfc8c396a556192ea1a674c6bfc4
Marlon <brow5187@gmail.com>
2025-01-19 22:23:30 +0800

    complete set of tutorials before working on To-Do App

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,17 +2,48 @@ import {useState} from "react";
 
 export default function Form() {
 
-    const [name, setName] = useState('')
+    //SINGLE INPUT
+        // const [name, setName] = useState('')
 
-    function handleChange() {
-        console.log("Change")
+    //MULTIPLE INPUTS
+    const [name, setName] = useState({firstName: "", lastName: ""});
+
+    //SINGLE INPUT
+        // function handleChange(e) {
+        //     console.log(e.target.value)
+        //     setName(e.target.value)
+        // }
+
+        // function demo(e) {
+        //     return handleChange(e);
+        // }
+
+    //MULTIPLE INPUTS
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(name);
     }
 
     return (
         <div>
-
+            {name.firstName} {name.lastName}
+            <br/><br/>
             <form>
-                <input onChange={handleChange} type="text" value={name} placeholder="First Name" />
+
+                {/*// SINGLE INPUT*/}
+                {/*<input onChange={(e) => setName(e.target.value)}*/}
+                {/*   type="text" value={name} placeholder="First Name" />*/}
+
+                <input onChange={
+                    (e) => setName({...name, firstName: e.target.value})
+                }
+                       type="text" value={name.firstName} placeholder="First Name"/>
+
+                <input onChange={
+                    (e) => setName({...name, lastName: e.target.value})
+                }
+                       type="text" value={name.lastName} placeholder="last Name"/><br/><br/>
+                <button onClick={(e) => handleSubmit(e)}>Add</button>
             </form>
 
         </div>
